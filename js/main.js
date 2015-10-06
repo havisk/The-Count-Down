1f89f4adb057ae53d4299ae2336244f800219361
Kool Havis <havisk@gmail.com>
2015-10-06 00:52:25 -0400

    finished clock 1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+setInterval( function() {
+	// Create a newDate() object and extract the seconds of the current time on the visitor's
+	var seconds = new Date().getSeconds();
+	// Add a leading zero to seconds value
+	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
+	},1000);
+	
