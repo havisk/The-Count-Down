ba16bad12935c44ea6953b7d3c425aebc8a29609
Kool Havis <havisk@gmail.com>
2015-10-06 02:48:56 -0400

    hex clock

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
-setInterval( function() {
+window.setInterval(function() {
 	// Create a newDate() object and extract the seconds of the current time on the visitor's
 	var seconds = new Date().getSeconds();
 	// Add a leading zero to seconds value
 	$("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
 	},1000);
 	
+
+
+
+// window.setInterval(function() {
+// 	// Create a newDate() object and extract the seconds of the current time on the visitor's
+// 	var seconds = new Date().getSeconds();
+// 	// Add a leading zero to seconds value
+// 	$("#").html(( seconds < 10 ? "0" : "" ) + seconds);
+// 	},1000);
+
+
+//show time
+var s, color;
+
+function showTime(){
+	d = newDate(); //new object
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
+
+	//add 0 to single digits
+	if(h <= 9) h = '0' +h;
+	if(m <= 9) m = '0' +m;
+	if(s <= 9) s = '0' +s;
+
+
+color = '#'+'8'+'45'+s;
+
+//background color
+document.querySelector(".box") = color
+
+//set time
+document.getElementById('hex').innerHTML = color
+
+//function timeout
+window.setTimout(showTime, 2000);
+
+}
+
+//call function
+showTime();
